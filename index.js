f520a08eca11b65a6f9a6e2548e1e3056e85b08f
Helen Liutongco <bellpickle@gmail.com>
2017-11-06 06:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(ternary){
-  let test2;
-  (ternary === "NYC") ? test2="Ok, sounds good." : test2="No go.";
+  let test=(ternary === "NYC") ? "Ok, sounds good." : "No go.";
   return test2;
 }
 
